80ecdf7e9b72ad0d79ebd439ee227eaa9225ab17
Sergio Gómez <hola@sergio-gomez.com>
2015-12-29 13:17:01 +0100

    Using mandrill for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'app.imput.io' }
   config.action_controller.asset_host = 'app.imput.io'
   config.action_mailer.asset_host = 'https://app.imput.io'
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"]
+  }
 end
